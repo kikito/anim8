a0f387d0f4aefbeffc3c992d50e39ceb8b7c9500
Enrique García Cota <kikito@gmail.com>
2012-03-01 00:14:57 +0100

    anim8.newAnimation works very well - delay parsing is awesome

--- anim8.lua
+++ anim8.lua
@@ -4,13 +4,13 @@
 -- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 -- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+local Grid = {}
+
 local function assertPositiveInteger(value, name)
   if type(value) ~= 'number' then error(("%s should be a number, was %q"):format(name, tostring(value))) end
   if value < 1 then error(("%s should be a positive number, was %d"):format(name, value)) end
   if value ~= math.floor(value) then error(("%s should be an integer, was %d"):format(name, value)) end
 end
-
-local Grid = {}
 
 local function createFrame(self, x, y)
   return love.graphics.newQuad(
@@ -120,9 +120,82 @@ end
 
 local Animation = {}
 
+local function cloneArray(arr)
+  local result = {}
+  for i=1,#arr do result[i] = arr[i] end
+  return result
+end
+
+local animationModes = {
+  loop   = function(self) self.position = 1 end,
+  once   = function(self) self.position = #self.frames end,
+  bounce = function(self)
+    self.direction = self.direction * -1
+    self.position = self.position + self.direction + self.direction
+  end
+}
+
+local Animationmt = { __index = Animation }
+
+local function parseDelays(delays)
+  local parsedDelays = {}
+  local tk,min,max
+  for k,v in pairs(delays) do
+    tk = type(k)
+    if     tk == "number"
+      then parsedDelays[k] = v
+    elseif tk == "string" then
+      min, max = parseInterval(k)
+      for i = min,max do parsedDelays[i] = v end
+    else
+      error(("Unexpected delay key: [%s]. Expected a number or a string"):format(tostring(k)))
+    end
+  end
+  return parsedDelays
+end
+
+local function repeatValue(value, times)
+  local result = {}
+  for i=1,times do result[i] = value end
+  return result
+end
+
+local function createDelays(frames, defaultDelay, delays)
+  local maxFrames = #frames
+  local result = repeatValue(defaultDelay, maxFrames)
+  for i,v in pairs(parseDelays(delays)) do
+    if i > maxFrames then
+      error(("The delay value %d is too high; there are only %d frames"):format(i, maxFrames))
+    end
+    result[i] = v
+  end
+  return result
+end
+
+local function newAnimation(mode, frames, defaultDelay, delays)
+  delays = delays or {}
+  assert(animationModes[mode], ("%q is not a valid mode"):format(tostring(mode)))
+  assert(type(defaultDelay) == 'number' and defaultDelay > 0, "defaultDelay must be a positive number" )
+  assert(type(delays) == 'table', "delays must be a table or nil")
+  return setmetatable({
+      mode        = mode,
+      frames      = cloneArray(frames),
+      padPosition = animationModes[mode],
+      delays      = createDelays(frames, defaultDelay, delays),
+      timer       = 0,
+      position    = 1,
+      direction   = 1,
+      status      = "playing"
+    },
+    Animationmt
+  )
+end
+
+
 -----------------------------------------------------------
 
 local anim8 = {
-  newGrid = newGrid
+  newGrid      = newGrid,
+  newAnimation = newAnimation
 }
 return anim8
