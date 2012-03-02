b00a46180db788f25a08de40455f71b8f0ad9737
Enrique García Cota <kikito@gmail.com>
2012-03-02 01:32:32 +0100

    it works!

--- main.lua
+++ main.lua
@@ -4,18 +4,41 @@ local anim8 = require 'anim8'
 function love.load()
   image = love.graphics.newImage('media/1945.png')
 
-  local g = anim8.newGrid(32, 32, 1024, 768, 1)
+  local g32 = anim8.newGrid(32, 32, 1024, 768, 3, 3, 1)
 
-  animations = {
-    anim8.newAnimation('loop', g('5-8,1', '1-2,1'), 0.1)
+  spinning = {
+    anim8.newAnimation('loop',   g32('5-8,1', '1-2,1'), 0.1),
+
+    anim8.newAnimation('bounce', g32('18,7-11'), 0.2),
+
+    anim8.newAnimation('loop',   g32('5-8,2', '1-2,2'), 0.2),
+
+    anim8.newAnimation('bounce', g32('19,7-11'), 0.2),
+
+    anim8.newAnimation('loop',   g32('5-8,3', '1-2,3'), 0.3),
+
+    anim8.newAnimation('bounce', g32('20,7-11'), 0.2),
+
+    anim8.newAnimation('loop',   g32('5-8,4', '1-2,4'), 0.4),
+
+    anim8.newAnimation('bounce', g32('21,7-11'), 0.2),
+
+    anim8.newAnimation('loop',   g32('5-8,5', '1-2,5'), 0.5),
+
   }
+
+  local g
 
 end
 
 function love.draw()
-  animations[1]:draw(image, 100, 100)
+  for i=1,#spinning do
+    spinning[i]:draw(image, i*75, i*50)
+  end
 end
 
 function love.update(dt)
-  animations[1]:update(dt)
+  for i=1,#spinning do
+    spinning[i]:update(dt)
+  end
 end
