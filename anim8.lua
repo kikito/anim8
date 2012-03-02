7312d93dfc3c6e2f6791ff98f6679df05970bb6e
Enrique García Cota <kikito@gmail.com>
2012-03-02 17:56:05 +0100

    updated lua version to 1.0. Finished README

--- anim8.lua
+++ anim8.lua
@@ -1,4 +1,4 @@
--- anim8 v0.5 - 2012-02
+-- anim8 v1.0.0 - 2012-02
 -- Copyright (c) 2011 Enrique García Cota
 -- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 -- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
